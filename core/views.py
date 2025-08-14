3f41f6e76526c6b5a964453d249b66bd1b2c9b13
manish <24m0800@iitb.ac.in>
2025-08-14 14:14:29 +0530

    Eliminated redundancy with help of get_or_create

--- core/views.py
+++ core/views.py
@@ -4,19 +4,15 @@ from .models import Link
 
 # Create your views here.
 def home_view(request):
-    """
-    View to create a new short URL.
-    """
-    shortend_url = ""
+    shortened_url = ""
     if request.method == 'POST':
         original_url = request.POST.get('original_url')
         if original_url:
-            # create a new Link object and save it
-            link = Link.objects.create(original_url = original_url)
-            # Build the full shortend URL to display to the user
-            shortend_url = request.build_absolute_uri('/') + link.short_code
-
-    return render(request, 'core/index.html', {'shortened_url': shortend_url})
+            # This line gets an existing link or creates a new one
+            link, created = Link.objects.get_or_create(original_url=original_url)
+            shortened_url = request.build_absolute_uri('/') + link.short_code
+    
+    return render(request, 'core/index.html', {'shortened_url': shortened_url})
 
 
 def redirect_view(request, short_code):
--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import optparse
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = "django-insecure-jrm15pgqwm+(&zu$h$b9f-*$$aq#!kzkboxpg*ch$ev_hp_3%j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['192.168.1.121', '127.0.0.1', 'localhost']
 
